4d09a563633aceedb317d1205c8c28c10b6fed2b
Calvin <calvin.peak@capslock.tw>
2016-09-05 03:06:10 +0900

    - Complete counting function. - Update usage example in view controller.

--- Animation-Issue/ViewController.swift
+++ Animation-Issue/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import CHCubicBezier
 
 class ViewController: UIViewController {
     @IBOutlet weak var countingLabel: CountingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countingLabel.textFormatter = { (value: Double) -> String in
+            return "\(Int(value))"
+        }
+        countingLabel.easing = CubicBezier.Easing.EaseInOut
+        countingLabel.duration = 5
+        countingLabel.startValue = 50
+//        countingLabel.stopValue = 100
     }
     
     @IBAction func startCountingButtonClicked(_: AnyObject) {
